59d021cb1a50d4f1a22f741c4bd9f3e67e6e42c0
Carlos Ivanski <losbr62@gmail.com>
2024-11-27 14:17:47 -0300

    Diagram

--- streamlit_app/🏠Pagina_principal_app.py
+++ streamlit_app/🏠Pagina_principal_app.py
@@ -145,11 +145,23 @@ elif st.session_state["authentication_status"]:
 
         st.markdown(
             """
-        RESUMO DE COMO USAR AQUI
+        📖 Manual de Utilização:
+
+        📊 Coleta de Informações
+        No início, utilizamos um website chamado Streamlit, onde foi desenvolvido um serviço para coletar informações destinadas a um dashboard de disponibilidade de horários. Essas informações serão armazenadas em um arquivo Excel, que pode ser carregado diretamente através do site.
+
+        📅 Tabela de Disponibilidade
+        Com base nas informações coletadas, é gerada uma tabela de disponibilidade contendo as condições especificadas. Essa tabela pode ser exportada como um relatório no formato Excel.
+
+        🛠️ Planejador de Rotas
+        O projeto final consiste em um planejador de rotas, que utiliza todas as informações coletadas para alocar os professores em suas respectivas turmas, considerando as condições específicas de cada um. O resultado também pode ser exportado como um relatório no formato Excel.
+
+        📞 Adicional: Abertura de Chamados
+        O sistema conta com a funcionalidade adicional de abertura de chamados para solicitar ajuda, caso necessário.
         """
         )
 
-        login_image_path = "streamlit_app/images/gato.jpg"
+        login_image_path = "streamlit_app/images/diagram.png"
         login_image = load_image(login_image_path)
 
         st.markdown(
