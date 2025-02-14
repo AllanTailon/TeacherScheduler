1ddb9997826bfb64c8a335fb2e1f8d59be1bf537
AllanTailon <lopez.delfino13@gmail.com>
2025-02-14 17:13:02 -0300

    novas funçoes

--- streamlit_app/validador.py
+++ streamlit_app/validador.py
@@ -7,6 +7,7 @@ class validador:
 
         self.df_class = df_class
         self.df_teach = df_teach
+        self.teacher_alocated = self.df_class[((self.df_class['teacher']!='-')&(self.df_class['teacher'].notnull()))]['teacher'].unique()
 
     def check_existent_teacher(self):
         """
@@ -37,7 +38,7 @@ class validador:
         horarios_nao_permitido = {}
         dia_da_semana_nao_permitido = {}
 
-        for professor in self.df_class[((self.df_class['teacher']!='-')&(self.df_class['teacher'].notnull()))]['teacher'].unique():
+        for professor in self.teacher_alocated:
             horarios = self.df_class[self.df_class['teacher']==professor]['horario'].unique()
             dia_semana = self.df_class[self.df_class['teacher']==professor]['dias da semana'].unique()
 
@@ -63,9 +64,38 @@ class validador:
         """
         Check if there are multiple classes in the same hour
         """
-        for professor in self.df_class[((self.df_class['teacher']!='-')&(self.df_class['teacher'].notnull()))]['teacher'].unique():
+        for professor in self.teacher_alocated:
             for diasemana in self.df_class[self.df_class['teacher']==professor]['dias da semana'].unique():
                 for horario in self.df_class[self.df_class['teacher']==professor]['horario'].unique():
                     turmas = self.df_class[(self.df_class['teacher']==professor)&(self.df_class['dias da semana']==diasemana)&(self.df_class['horario']==horario)]['nome grupo'].unique()
                     if len(turmas)>1:
                         print(f'Teacher {professor} has multiple classes in the same hour: {turmas}')
+    
+    def check_modality_group(self):
+        """
+        
+        """ 
+        modalidades = self.df_class[self.df_class['modalidade']!='Inglês']['modalidade'].unique()
+        grupo = self.df_class['grupo'].unique()
+
+        colunas_prof = self.df_teach.columns
+        diff_mod = set(modalidades) - set(colunas_prof)
+        diff_grupo = set(grupo) - set(colunas_prof)
+        aulas_mod = self.df_class[self.df_class['modalidade'].isin(diff_mod)]['nome grupo'].unique()
+        aulas_grupo = self.df_class[self.df_class['grupo'].isin(diff_grupo)]['nome grupo'].unique()
+        if diff_mod:
+            print(f'Modalities not found in the teacher table: {diff_mod} para a seguintes turmas: {aulas_mod}')
+        if diff_grupo:
+            print(f'Groups not found in the teacher table: {diff_grupo} para a seguintes turmas: {aulas_grupo}')
+
+    def check_teach_status(self):
+        """
+        Check if the teacher is active
+        """
+        for i in self.teacher_alocated:
+            if i in self.df_teach['TEACHER'].unique():
+                status = self.df_class[self.df_class['teacher']==i]['status'].unique()
+                for s in status:
+                    if self.df_teach[self.df_teach['TEACHER']==i][s].values[0] == 0:
+                        print(f'Teacher {i} cannot teach in status {s}')
+            
